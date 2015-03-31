da4b94a7c4893eae46dd7514788a419455ac0f00
kepton <liuyalongscu@foxmail.com>
2015-03-31 16:28:36 +0800

    First Commit

---
+++ Client/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Service1Client client = new Service1Client();
+            string msg = client.GetData(12);
+            Console.WriteLine(msg);
+            Console.ReadKey();
+        }
+    }
+}
